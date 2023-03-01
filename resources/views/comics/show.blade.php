06c5afdf0207d51760c3b2f2832c38c965892109
MindulaHewadewage <mindufernando@gmail.com>
2023-03-01 20:21:23 +0100

    spec (da fixare)

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,22 +6,58 @@
 @section('content')
 <div class="comics-jumbo"></div>
 <div class="container">
-<section class="description">
+<section class="comic-preview">
 <div class="section-comic-description">
+    <img class="img-preview" src="{{$comic->thumb}}" alt="">
     <h1 class="comic-title">{{$comic->title}}</h1>
     <div class="comic-price">
        <p>US. Price: {{$comic->price}}</p>
     </div>
     <p class="comic-description">{{$comic->description}}</p>
-    
 </div>
 
 <div class="ad">
     <img src="{{Vite::asset('resources/img/placeholder.png')}}" alt="adv">
 </div>
 </section>
-
 </div>
+
+<div class="light-bg">
+<section class="comic-spec-table container">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody> 
+          <tr>
+            <th scope="row">Art By:</th>
+            <td>{{$comic->artists}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Written By:</th>
+            <td>{{$comic->writers}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Series:</th>
+            <td>{{$comic->series}}</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Us price:</th>
+            <td>{{$comic->price}}</td>
+          </tr>
+
+          <tr>
+            <th scope="row">On sale Date:</th>
+            <td>{{$comic->sale_date}}</td>
+          </tr>
+        </tbody>
+      </table>
+</section>
+</div>
+
 
 
 @endsection
