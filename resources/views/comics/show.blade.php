1ab45e495103d7b163d3cc05315fa638ac03125b
MindulaHewadewage <mindufernando@gmail.com>
2023-03-01 19:40:11 +0100

    fisr comic description

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,8 +4,23 @@
 
 
 @section('content')
-
+<div class="comics-jumbo"></div>
+<div class="container">
+<section class="description">
+<div class="section-comic-description">
+    <h1 class="comic-title">{{$comic->title}}</h1>
+    <div class="comic-price">
+       <p>US. Price: {{$comic->price}}</p>
+    </div>
+    <p class="comic-description">{{$comic->description}}</p>
+    
 </div>
+
+<div class="ad">
+    <img src="{{Vite::asset('resources/img/placeholder.png')}}" alt="adv">
+</div>
+</section>
+
 </div>
 
 
